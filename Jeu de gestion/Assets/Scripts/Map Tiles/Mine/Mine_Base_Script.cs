2ef7e212f779873e3e6a2cad4c181096750fa1ac
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-03-03 22:40:56 -0500

    Élaboration du script et des sprites
    Le script de la mine est en cours, ainsi que ses sprites. Il va s'agir tout d'abord de sprites de très basse qualité, jusqu'aux premières versions Alpha pour ainsi permettre de développer le jeu plus rapidement. Pour ce qui est du script en cours, il s'agit du script d'affichage de la mine (Gestion des sprites et tout et tout...)

--- Jeu de gestion/Assets/Scripts/Map Tiles/Mine/Mine_Base_Script.cs
+++ Jeu de gestion/Assets/Scripts/Map Tiles/Mine/Mine_Base_Script.cs
@@ -4,9 +4,58 @@ using UnityEngine;
 
 public class Mine_Base_Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    GameObject PrimaryOreSpriteHandler;
+
+    //Liste de tous les sprites nécessaires pour le bon fonctionnement de la mine toute entière
+
+    [SerializeField]
+    Sprite BaseMineSprite;
+
+    //Liste des sprites pour le fer
+    [SerializeField]
+    Sprite IronPrimaryMineSprite;
+    [SerializeField]
+    Sprite IronSecondaryMineSprite;
+    [SerializeField]
+    Sprite IronOreSprite;
+
+    //Liste des sprites pour le cuivre
+    [SerializeField]
+    Sprite CopperPrimaryMineSprite;
+    [SerializeField]
+    Sprite CopperSecondaryMineSprite;
+    [SerializeField]
+    Sprite CopperOreSprite;
+
+    //Liste des sprites pour l'étain
+    [SerializeField]
+    Sprite TinPrimaryMineSprite;
+    [SerializeField]
+    Sprite TinSecondaryMineSprite;
+    [SerializeField]
+    Sprite TinOreSprite;
+
+    //Liste des états de la mine
+    [SerializeField]
+    Sprite UnknownMineSprite;
+    [SerializeField]
+    Sprite PausedMineSprite;
+    [SerializeField]
+    Sprite InProgressMineSprite;
+    [SerializeField]
+    Sprite EmptyMineSprite;
+
+    //Liste des sprites de progression pour la construction de la mine
+    [SerializeField]
+    Sprite BuildingFirstPartSprite;
+    [SerializeField]
+    Sprite BuildingSecondPartSprite;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.GetComponent<SpriteRenderer>().sprite = BaseMineSprite;
+        PrimaryOreSpriteHandler.GetComponent<SpriteRenderer>().sprite = CopperPrimaryMineSprite;
 	}
 	
 	// Update is called once per frame
